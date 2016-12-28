1977b200c9eab0dd572d7260617382bac72bdbd0
chibi929 <anbi929.dev@gmail.com>
2016-12-28 23:28:47 +0900

    Update README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# .emacs.d
+
+## Environment
+
+- Cask
+
+## Setup
+
+```
+$ git clone git@github.com:chibi929/.emacs.d.git ~/.emacs.d
+$ cd ~/.emacs.d
+$ cask
+```
--- inits/63_coffee-mode.el
+++ inits/63_coffee-mode.el
@@ -5,6 +5,6 @@
 (defun setup-coffee-mode ()
   (setq tab-width 2)
   (setq coffee-tab-width 2)
-  (add-to-list 'ac-dictionary-files "~/.emacs.d/elpa/auto-complete-20160710.1544/dict/js2-mode")
-)
+  (add-to-list 'ac-dictionary-files "~/.emacs.d/.cask/24.5/elpa/js2-mode-20161212.1716/js2-mode")
+  )
 (add-hook 'coffee-mode-hook 'setup-coffee-mode)
