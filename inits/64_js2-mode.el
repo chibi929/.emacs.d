54dcd00f914110cff13eff633b387e5c24d863f3
chibi929 <anbi929.dev@gmail.com>
2017-03-25 10:57:33 +0900

    Add tern package.

--- inits/64_js2-mode.el
+++ inits/64_js2-mode.el
@@ -3,7 +3,10 @@
 ;;==============================
 
 (defun setup-js2-mode ()
+  (setq company-tern-property-marker "")
+  (set (make-local-variable 'company-backends) '(company-tern))
   (setq tab-width 2)
   (setq js2-basic-offset 2)
-)
+  )
 (add-hook 'js2-mode-hook 'setup-js2-mode)
+(add-hook 'js2-mode-hook 'tern-mode)
